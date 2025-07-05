96990fcac27877dcac65ef046c95584067e6e195
MagChap <magnuslchapple@gmail.com>
2025-07-05 18:15:04 +0100

    First commit with README (empty) and generator js file with three arrays

---
+++ generator.js
@@ -0,0 +1,6 @@
+const grape = ["Pinot Noir", "Chardonnay", "Cabernet Sauvignon", "Sauvignon Blanc", "Syrah", "Riesling", "Merlot", "Chenin Blanc"]
+
+const country = ["French", "German", "American", "Australian", "English", "Kiwi", "Chilean", "South African"]
+
+const vintage = ["2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017"]
+
